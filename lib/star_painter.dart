1a67f61db1b9537e4c9850313df71059cc6c4d48
Түвшинжаргал <53771399+atuvshinjargal@users.noreply.github.com>
2023-06-20 14:30:54 +0800

    secondary

--- lib/star_painter.dart
+++ lib/star_painter.dart
@@ -10,9 +10,19 @@ class StarPainter extends CustomPainter{
     final paint = Paint()..color = color; //ungu tohiruulah
 
     final path = Path();
-    path.moveTo(size.width/2, 0);
+    path.moveTo(size.width*0.5, 0);
     path.lineTo(size.width*0.618, size.height*0.382);
+    path.lineTo(size.width, size.height*0.382);
     path.lineTo(size.width*0.382, size.height*0.618);
+    path.lineTo(size.width, size.height*0.382);
+    path.lineTo(size.width*0.691, size.height*0.618);
+    path.lineTo(size.width*0.809, size.height);
+    path.lineTo(size.width*0.5, size.height*0.7639);
+    path.lineTo(size.width*0.191, size.height);
+    path.lineTo(size.width*0.309, size.height*0.618);
+    path.lineTo(0, size.height*0.382);
+    path.lineTo(size.width*0.382, size.height*0.382);
+
     path.close();
     canvas.drawPath(path, paint);
   }
