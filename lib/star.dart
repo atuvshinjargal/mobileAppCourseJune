6b72d01fa07eeb1fa1a83450cf9ba88867dce85a
Түвшинжаргал <53771399+atuvshinjargal@users.noreply.github.com>
2023-06-20 13:55:42 +0800

    first com

---
+++ lib/star.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Star extends StatelessWidget {
+  final double size; //double gedeg n butarthai too 37.5 geh met
+  final Color color; //Ungu jisheen RGB(255,124,65)
+  const Star({
+    super.key,
+    required this.color, //reqiured zaaval baih shaardlagatai
+    required this.size, //reqiured zaaval baih shaardlagatai
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: size,
+      height: size,
+      child: CustomPaint(
+        painter: _StarPainter(),
+      ),
+    );
+  }
+}
