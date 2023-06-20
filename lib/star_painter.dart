21794bef289caba3c98315799532bbc3714f3cef
Түвшинжаргал <53771399+atuvshinjargal@users.noreply.github.com>
2023-06-20 14:04:08 +0800

    secondary

--- lib/star_painter.dart
+++ lib/star_painter.dart
@@ -12,7 +12,7 @@ class StarPainter extends CustomPainter{
     final path = Path();
     path.moveTo(size.width/2, 0);
     path.lineTo(size.width*0.618, size.height*0.382);
-
+    path.lineTo(size.width*0.382, size.height*0.618);
     path.close();
     canvas.drawPath(path, paint);
   }
