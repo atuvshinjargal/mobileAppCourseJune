380c8cc8ce9f5bfdcacd3d58ca6a2f88cd10da45
Түвшинжаргал <53771399+atuvshinjargal@users.noreply.github.com>
2023-06-21 13:21:20 +0800

    suuld

--- lib/ecommerce_screen.dart
+++ lib/ecommerce_screen.dart
@@ -8,7 +8,59 @@ class ECommerceScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).primaryColor,
-        leading: Icon(Icons.home,),
+        leading: Icon(
+          Icons.home,
+        ),
+        title: Center(
+          child: Text('Lets go shopping'),
+        ),
+        actions: [
+          Icon(
+            Icons.shopping_cart,
+          )
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 20, //hooson zai avah
+            ),
+            Container(
+              child: Row(
+                children: [
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      width: double.infinity,
+                      child: Center(
+                        child: Text('Recommended'),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      width: double.infinity,
+                      child: Center(
+                        child: Text('Formal Wear'),
+                      ),
+                    ),
+                  ),
+                  Flexible(
+                    flex: 1,
+                    child: Container(
+                      width: double.infinity,
+                      child: Center(
+                        child: Text('Casual Wear'),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
